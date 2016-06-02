f5933038906f817209d81ebe32f31cb197cead6a
Himal Mandalia <himalmandalia@gmail.com>
2016-06-02 09:22:57 +0100

    Sample Doorkeeper applications

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Register sample applications for the Doorkeeper OAuth2 provider
+
+pvb = Doorkeeper::Application.create!(
+  name: 'Prison Visits Booking',
+  redirect_uri: 'http://localhost:3000'
+)
+
+mtp = Doorkeeper::Application.create!(
+  name: 'Money to Prisoners',
+  redirect_uri: 'http://localhost:3000'
+)
+
+mps = Doorkeeper::Application.create!(
+  name: 'Moving People Safely',
+  redirect_uri: 'http://localhost:3000'
+)
