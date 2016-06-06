419ac5c2f319ae40e8b11baff50584475a6da5fd
Himal Mandalia <himalmandalia@gmail.com>
2016-06-06 16:03:44 +0100

    Prisoner API controller specs

--- spec/controllers/prisoners_controller_spec.rb
+++ spec/controllers/prisoners_controller_spec.rb
@@ -1,23 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe PrisonersController, type: :controller do
-  describe 'GET #index' do
+  let!(:application) { create(:application) }
+  let!(:token)       { create(:access_token, application: application) }
 
+  context 'when authenticated' do
+    before { request.headers['HTTP_AUTHORIZATION'] = "Bearer #{token.token}" }
+
+    describe 'GET #index' do
+      before { get :index }
+
+      it 'returns status 200' do
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe 'GET #show' do
+      before { get :show, id: 1 }
+
+      it 'returns status 200' do
+        expect(response.status).to eq(200)
+      end
+    end
+
+    describe 'POST #create' do
+
+    end
+
+    describe 'PATCH #update' do
+
+    end
+
+    describe 'DELETE #destroy' do
+
+    end
   end
 
-  describe 'GET #show' do
+  context 'when unauthenticated' do
+    describe 'GET #index' do
+      before { get :index }
 
-  end
+      it 'returns status 401' do
+        expect(response.status).to eq(401)
+      end
+    end
 
-  describe 'POST #create' do
+    describe 'GET #show' do
+      before { get :show, id: 1 }
 
-  end
+      it 'returns status 401' do
+        expect(response.status).to eq(401)
+      end
+    end
 
-  describe 'PATCH #update' do
+    describe 'POST #create' do
 
-  end
+    end
 
-  describe 'DELETE #destroy' do
+    describe 'PATCH #update' do
 
+    end
+
+    describe 'DELETE #destroy' do
+
+    end
   end
 end
