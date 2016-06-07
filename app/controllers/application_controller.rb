9f08e7d3927f58248ff04a6aa3e1cacc0bc3a521
Himal Mandalia <himalmandalia@gmail.com>
2016-06-07 14:55:37 +0100

    Global doorkeep authorise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_action :doorkeeper_authorize!
+
   def doorkeeper_unauthorized_render_options(error: nil)
     { json: { error: "Not authorised" } }
   end
