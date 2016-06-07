f561800e8ff5d1e42989ca7f8b2d2027feae9ad2
Himal Mandalia <himalmandalia@gmail.com>
2016-06-07 14:56:01 +0100

    Do not user doorkeeper for home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  before_action :doorkeeper_authorize!, except: [:index]
+
   def index
     @applications = Doorkeeper::Application.order(name: :asc)
   end
