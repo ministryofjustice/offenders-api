684e5b2ff490464d7f044b57bad86d2879917661
Himal Mandalia <himalmandalia@gmail.com>
2016-06-08 15:48:47 +0100

    Scaffold CRUD actions for services

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -2,33 +2,52 @@ class ServicesController < ApplicationController
   before_action :set_service, only: [:show, :edit, :update, :destroy]
 
   def index
-    @applications = Doorkeeper::Application.order(name: :asc)
+    @services = Doorkeeper::Application.order(name: :asc)
   end
 
   def show
   end
 
   def new
+    @service = Doorkeeper::Application.new
   end
 
   def edit
   end
 
   def create
+    @service = Doorkeeper::Application.new(service_params)
 
+    if @service.save
+      redirect_to services_url, notice: 'Service added'
+    else
+      render :new
+    end
   end
 
   def update
-
+    if @service.update(service_params)
+      redirect_to services_url, notice: 'Service updated'
+    else
+      render :edit
+    end
   end
 
   def destroy
-
+    @service.destroy
+    redirect_to services_url, notice: 'Service removed'
   end
 
   private
 
   def set_service
-    @application = Doorkeeper::Application.find(params[:id])
+    @service = Doorkeeper::Application.find(params[:id])
+  end
+
+  def service_params
+    params.require(:service).permit(
+     :name,
+     :redirect_uri
+    )
   end
 end
--- spec/controllers/services_controller_spec.rb
+++ spec/controllers/services_controller_spec.rb
@@ -16,4 +16,53 @@ RSpec.describe ServicesController, type: :controller do
       expect(response).to render_template(:index)
     end
   end
+
+  describe 'GET #show' do
+    let(:application) { create(:application) }
+
+    before { get :show, id: application }
+
+    it 'should return status 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'should render the show template' do
+      expect(response).to render_template(:show)
+    end
+  end
+
+  describe 'GET #new' do
+    before { get :new }
+
+    it 'should return status 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'should render the new template' do
+      expect(response).to render_template(:new)
+    end
+  end
+
+  describe 'GET #edit' do
+    let(:application) { create(:application) }
+
+    before { get :edit, id: application }
+
+    it 'should return status 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'should render the index template' do
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe 'POST #create' do
+  end
+
+  describe 'PATCH/PUT #update' do
+  end
+
+  describe 'DELETE #destroy' do
+  end
 end
