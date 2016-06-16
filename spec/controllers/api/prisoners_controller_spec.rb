92518f212942eb82129298f6f16a840d191a0716
Himal Mandalia <himalmandalia@gmail.com>
2016-06-16 11:47:15 +0100

    PrisonersController#search specs

--- spec/controllers/api/prisoners_controller_spec.rb
+++ spec/controllers/api/prisoners_controller_spec.rb
@@ -16,6 +16,53 @@ RSpec.describe Api::PrisonersController, type: :controller do
       end
     end
 
+    describe 'GET #search' do
+      context 'missing required params' do
+        before { get :search }
+
+        it 'should return status 400' do
+          expect(response.status).to eq(400)
+        end
+
+        it 'returns JSON with error message' do
+          expect(JSON.parse(response.body)['error']).to eq('NOMS ID or date of birth not present')
+        end
+      end
+
+      context 'required params present' do
+        let(:prisoner) { create(:prisoner, noms_id: 'A1234BC', date_of_birth: Date.parse('19801010')) }
+        let(:params) { { noms_id: prisoner.noms_id, date_of_birth: prisoner.date_of_birth }}
+
+        before { get :search, params }
+
+        context 'no matching record found' do
+          let(:params) { { noms_id: 'A999ZZ', date_of_birth: Date.today }}
+
+          it 'returns status 200' do
+            expect(response.status).to eq(200)
+          end
+
+          it 'returns JSON signifying record not found' do
+            expect(JSON.parse(response.body)).to eq({ 'found' => false })
+          end
+        end
+
+        context 'matching record found' do
+          let(:expected_response) do
+            { 'found' => true, 'offender' => { 'id' => prisoner.offender_id } }
+          end
+
+          it 'returns status 200' do
+            expect(response.status).to eq(200)
+          end
+
+          it 'returns JSON signifying record found with offender id' do
+            expect(JSON.parse(response.body)).to eq(expected_response)
+          end
+        end
+      end
+    end
+
     describe 'GET #show' do
       let(:prisoner) { create(:prisoner) }
 
