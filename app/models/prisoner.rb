f95abe31c325569170864de85762dc3bbbeaa9a5
Himal Mandalia <himalmandalia@gmail.com>
2016-06-27 14:50:15 +0100

    Disable validations

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -1,7 +1,3 @@
 class Prisoner < ActiveRecord::Base
   has_many :aliases, dependent: :destroy
-
-  validates :offender_id, uniqueness: { case_sensitive: false }
-  validates :noms_id, uniqueness: { case_sensitive: false }
-  validates :pnc_number, uniqueness: { case_sensitive: false }
 end
--- spec/models/prisoner_spec.rb
+++ spec/models/prisoner_spec.rb
@@ -2,8 +2,4 @@ require 'rails_helper'
 
 RSpec.describe Prisoner, type: :model do
   it { should have_many(:aliases) }
-
-  it { should validate_uniqueness_of(:offender_id) }
-  it { should validate_uniqueness_of(:noms_id) }
-  it { should validate_uniqueness_of(:pnc_number) }
 end
