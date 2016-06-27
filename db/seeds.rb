6549ebdf847699cf68891d608d2155ed0792c9d1
Himal Mandalia <himalmandalia@gmail.com>
2016-06-27 14:51:03 +0100

    Disable seed prisoners

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,29 +20,29 @@ mps = Doorkeeper::Application.create!(
 User.create!(email: 'example@example.com', password: 'password', password_confirmation: 'password')
 
 # Sample Prisoner records
-
-p = Prisoner.create!(
-  given_name: 'John',
-  surname: 'Smith',
-  offender_id: '1',
-  noms_id: 'A1234AA',
-  date_of_birth: '19911010'
-)
-p.aliases.build(name: 'Johnny')
-p.save!
-
-Prisoner.create!(
-  given_name: 'Bob',
-  surname: 'Jones',
-  offender_id: '2',
-  noms_id: 'A1234AB',
-  date_of_birth: '19810304'
-)
-
-Prisoner.create!(
-  given_name: 'Keith',
-  surname: 'Johnson',
-  offender_id: '3',
-  noms_id: 'A1234AC',
-  date_of_birth: '19760821'
-)
+#
+# p = Prisoner.create!(
+#   given_name: 'John',
+#   surname: 'Smith',
+#   offender_id: '1',
+#   noms_id: 'A1234AA',
+#   date_of_birth: '19911010'
+# )
+# p.aliases.build(name: 'Johnny')
+# p.save!
+#
+# Prisoner.create!(
+#   given_name: 'Bob',
+#   surname: 'Jones',
+#   offender_id: '2',
+#   noms_id: 'A1234AB',
+#   date_of_birth: '19810304'
+# )
+#
+# Prisoner.create!(
+#   given_name: 'Keith',
+#   surname: 'Johnson',
+#   offender_id: '3',
+#   noms_id: 'A1234AC',
+#   date_of_birth: '19760821'
+# )
