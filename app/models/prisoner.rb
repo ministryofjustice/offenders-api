c06c62cd7bc2c757ce3221bb180a74d868bd409f
Himal Mandalia <himalmandalia@gmail.com>
2016-06-27 14:38:08 +0100

    Uniqueness validations for key prisoner attributes

--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -1,19 +1,7 @@
 class Prisoner < ActiveRecord::Base
-  # field :noms_id, type: String
-  # field :offender_id, type: String
-  # field :given_name, type: String
-  # field :middle_names, type: String
-  # field :surname, type: String
-  # field :title, type: String
-  # field :suffix, type: String
-  # field :date_of_birth, type: Date
-  # field :gender, type: String
-  # field :pnc_number, type: String
-  # field :nationality, type: String
-  # field :ethnicity, type: String
-  # field :languages, type: Array
-  # field :requires_interpreter, type: Boolean
-  # field :sexual_orientation, type: String
-
   has_many :aliases, dependent: :destroy
+
+  validates :offender_id, uniqueness: { case_sensitive: false }
+  validates :noms_id, uniqueness: { case_sensitive: false }
+  validates :pnc_number, uniqueness: { case_sensitive: false }
 end
