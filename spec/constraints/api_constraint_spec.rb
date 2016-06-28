2d664e86e9c5aa870880bc71a4c0ba8d42c5c191
Himal Mandalia <himalmandalia@gmail.com>
2016-06-28 11:59:07 +0100

    Cleanup spec

--- spec/constraints/api_constraint_spec.rb
+++ spec/constraints/api_constraint_spec.rb
@@ -2,36 +2,28 @@ require 'rails_helper'
 
 RSpec.describe ApiConstraint do
   describe '#matches?' do
+    let(:options_version) { '1' }
+    let(:accept_version) { '1' }
     let(:request) { double }
+    let(:options) { { version: options_version } }
+    let(:returned_result) do
+      { accept: "version=#{accept_version}" }
+    end
+
+    subject { ApiConstraint.new(options) }
+
+    before :each do
+      allow(request).to receive(:headers).and_return(returned_result)
+    end
 
     context 'when version matches' do
-      let(:options) { { version: '1' } }
-      let(:returned_result) do
-        { accept: 'version=1' }
-      end
-
-      subject { ApiConstraint.new(options) }
-
-      before :each do
-        allow(request).to receive(:headers).and_return(returned_result)
-      end
-
       it 'returns true' do
         expect(subject.matches?(request)).to eq(true)
       end
     end
 
     context 'when version does not match' do
-      let(:options) { { version: '2' } }
-      let(:returned_result) do
-        { accept: 'version=1' }
-      end
-
-      subject { ApiConstraint.new(options) }
-
-      before :each do
-        allow(request).to receive(:headers).and_return(returned_result)
-      end
+      let(:accept_version) { '2' }
 
       it 'returns false' do
         expect(subject.matches?(request)).to eq(false)
