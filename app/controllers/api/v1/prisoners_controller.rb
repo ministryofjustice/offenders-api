5f9a05d2c92ee787049cd161a00e88162a5fb30c
Himal Mandalia <himalmandalia@gmail.com>
2016-07-04 15:51:29 +0100

    Constrain searching to name attributes

--- app/controllers/api/v1/prisoners_controller.rb
+++ app/controllers/api/v1/prisoners_controller.rb
@@ -4,7 +4,7 @@ module Api
       before_action :set_prisoner, only: [:show, :update, :destroy]
 
       def index
-        @prisoners = Prisoner.basic_search(params[:query])
+        @prisoners = Prisoner.search(params[:query])
 
         render json: @prisoners
       end
--- app/models/prisoner.rb
+++ app/models/prisoner.rb
@@ -1,3 +1,18 @@
 class Prisoner < ActiveRecord::Base
   has_many :aliases, dependent: :destroy
+
+  class << self
+    def search(term)
+      term.strip! rescue nil
+      basic_search(searchable_columns.inject({}) { |h, c| h[c] = term; h }, false)
+    end
+
+    def searchable_columns
+      [
+        :given_name,
+        :middle_names,
+        :surname
+      ]
+    end
+  end
 end
