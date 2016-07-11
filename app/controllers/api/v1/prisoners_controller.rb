ff96d1cbfc3aaa9d43a95364c22143050c900a45
Himal Mandalia <himalmandalia@gmail.com>
2016-07-11 16:15:09 +0100

    Tidy indentation

--- app/controllers/api/v1/prisoners_controller.rb
+++ app/controllers/api/v1/prisoners_controller.rb
@@ -35,20 +35,20 @@ module Api
 
       def prisoner_params
         params.require(:prisoner).permit(
-        :noms_id,
-        :given_name,
-        :middle_names,
-        :surname,
-        :title,
-        :suffix,
-        :date_of_birth,
-        :gender,
-        :pnc_number,
-        :nationality,
-        :second_nationality,
-        :ethnicity_code,
-        :sexual_orientation,
-        :cro_number
+          :noms_id,
+          :given_name,
+          :middle_names,
+          :surname,
+          :title,
+          :suffix,
+          :date_of_birth,
+          :gender,
+          :pnc_number,
+          :nationality,
+          :second_nationality,
+          :ethnicity_code,
+          :sexual_orientation,
+          :cro_number
         )
       end
 
