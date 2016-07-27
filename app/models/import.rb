31b876c9eedabe75f5f0bb7607b469361513fbdd
Himal Mandalia <himalmandalia@gmail.com>
2016-07-27 11:38:09 +0100

    Remove previous imports when new one is saved

--- app/models/import.rb
+++ app/models/import.rb
@@ -5,6 +5,7 @@ class Import < ActiveRecord::Base
   validates :md5, presence: true, uniqueness: true
 
   before_validation :generate_md5
+  after_create :remove_previous
 
   private
 
@@ -12,5 +13,9 @@ class Import < ActiveRecord::Base
     if file.present? && file_changed?
       self.md5 = file.md5
     end
+  end
+
+  def remove_previous
+    Import.where('id != ?', self.id).destroy_all
   end
 end
--- spec/models/import_spec.rb
+++ spec/models/import_spec.rb
@@ -5,8 +5,11 @@ RSpec.describe Import, type: :model do
   it { should validate_presence_of(:md5) }
   it { should validate_uniqueness_of(:md5) }
 
+  let(:sample_import_1) { fixture_file_upload('files/sample_import_1.csv', 'text/csv') }
+  let(:sample_import_2) { fixture_file_upload('files/sample_import_2.csv', 'text/csv') }
+
   describe 'md5' do
-    subject { Import.new(file: fixture_file_upload('files/sample_import.csv', 'text/csv')) }
+    subject { Import.new(file: sample_import_1) }
 
     before { subject.save! }
 
@@ -16,9 +19,22 @@ RSpec.describe Import, type: :model do
     end
 
     it 'should not save when same file used to create another import' do
-      import = Import.create(file: fixture_file_upload('files/sample_import.csv', 'text/csv'))
+      import = Import.create(file: sample_import_1)
       expect(Import.count).to eq(1)
       expect(import.errors.messages).to include(md5: ['has already been taken'])
+    end
+  end
+
+  describe 'remove previous imports' do
+    subject { Import.new(file: sample_import_1) }
+
+    before { subject.save! }
+
+    it 'should remove previous imports when a new one is created' do
+      expect(Import.count).to eq(1)
+      import = Import.create(file: sample_import_2)
+      expect(Import.count).to eq(1)
+      expect(Import.first).to eq(import)
     end
   end
 end
