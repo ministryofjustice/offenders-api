7eb1e608536eea8f84512760d72f24163e3a6d09
Himal Mandalia <himalmandalia@gmail.com>
2016-09-06 09:17:02 +0100

    Additional specs for .search

--- spec/models/prisoner_spec.rb
+++ spec/models/prisoner_spec.rb
@@ -22,6 +22,10 @@ RSpec.describe Prisoner, type: :model do
       create(:prisoner, noms_id: 'AB123', date_of_birth: Date.parse('19750201'))
     end
 
+    let!(:prisoner_3) do
+      create(:prisoner, noms_id: 'AC123', date_of_birth: Date.parse('19650201'))
+    end
+
     context 'params not present' do
       it 'returns an empty set' do
         expect(Prisoner.search(params)).to eq([])
@@ -29,14 +33,42 @@ RSpec.describe Prisoner, type: :model do
     end
 
     context 'params present' do
-      let(:params) do
-        [
-          { noms_id: 'AA123', date_of_birth: Date.parse('19750201') }
-        ]
+      context 'no matching records exist' do
+        let(:params) do
+          [
+            { noms_id: 'CC123', date_of_birth: Date.parse('19750201') }
+          ]
+        end
+
+        it 'returns matching records' do
+          expect(Prisoner.search(params)).to match_array([])
+        end
       end
 
-      it 'returns matching records' do
-        expect(Prisoner.search(params)).to eq([prisoner_1])
+      context 'searching for multiple records' do
+        let(:params) do
+          [
+            { noms_id: 'AA123', date_of_birth: Date.parse('19750201') },
+            { noms_id: 'AC123', date_of_birth: Date.parse('19650201') },
+            { noms_id: 'AZ123', date_of_birth: Date.parse('19650201') }
+          ]
+        end
+
+        it 'returns matching records' do
+          expect(Prisoner.search(params)).to match_array([prisoner_1, prisoner_3])
+        end
+      end
+
+      context 'searching for single record' do
+        let(:params) do
+          [
+            { noms_id: 'AC123', date_of_birth: Date.parse('19650201') }
+          ]
+        end
+
+        it 'returns matching record' do
+          expect(Prisoner.search(params)).to match_array([prisoner_3])
+        end
       end
     end
   end
