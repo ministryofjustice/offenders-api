ec676fb8d48a3edfb81aa4eedaa9e97837da3fd2
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2016-09-26 10:38:09 +0100

    Bundle update gems

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  ROLES = %w[ admin staff ]
+  ROLES = %w[admin staff].freeze
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
--- app/services/import_prisoners.rb
+++ app/services/import_prisoners.rb
@@ -7,7 +7,7 @@ module ImportPrisoners
     import.update_attribute(:status, :successful)
     Import.where('id != ?', import.id).destroy_all
   rescue StandardError => e
-    import.update_attribute(:status, :failed) if import
+    import&.update_attribute(:status, :failed)
     NotificationMailer.import_failed(import, e.to_s).deliver_now
   end
 end
