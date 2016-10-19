30016eae6088533f3a258ea31e4baa76c1ea0251
Himal Mandalia <himalmandalia@gmail.com>
2016-10-19 12:55:57 +0100

    Remove linked files from cap deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,6 @@ set :branch, 'azure-deploy'
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
 
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
-set :linked_files, fetch(:linked_files, []).push('config/secrets.yml')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
