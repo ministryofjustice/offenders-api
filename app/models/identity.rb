3a571e12e9d88c1e38b6004edda9033e80eb36fb
Cesidio Di Landa <cesidio.dilanda@gmail.com>
2016-12-05 11:06:50 +0000

    Search identities by exact surname

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -16,15 +16,17 @@ class Identity < ActiveRecord::Base
 
   def self.search(params)
     results = joins(:offender)
-    %i(given_name middle_names surname).each do |field|
-      next unless params[field]
-      term = params.delete(field)
-      results = results.where("#{field} ILIKE :term", term: "%#{term}%")
-    end
-    %i(noms_id nationality_code establishment_code).each do |field|
+    %i(given_name middle_names surname noms_id nationality_code establishment_code).each do |field|
       next unless params[field]
       value = params.delete(field)
-      results = results.where("offenders.#{field} = :value", value: value)
+      case field
+      when :given_name, :middle_names
+        results = results.where("#{field} ILIKE :term", term: "%#{value}%")
+      when :surname
+        results = results.where("#{field} ILIKE :term", term: value)
+      when :noms_id, :nationality_code, :establishment_code
+        results = results.where("offenders.#{field} = :value", value: value)
+      end
     end
     results.where(params).order(:surname, :given_name, :middle_names)
   end
