411a5e2d8744ce7c7f3ec0c6570105f4031e06f2
Himal Mandalia <himalmandalia@gmail.com>
2017-06-22 11:48:40 +0100

    Seeds sample apps only in development env, or on dev and staging hosts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,27 @@
-# Register sample applications for the Doorkeeper OAuth2 provider
-nomis = Doorkeeper::Application.find_or_create_by!(
-  name: 'NOMIS',
-  redirect_uri: "https://#{ENV['HTTP_HOST']}",
-  scopes: 'public write'
-)
+# Register sample applications for the Doorkeeper OAuth2 provider in development
 
-pvb = Doorkeeper::Application.find_or_create_by!(
-  name: 'Prison Visits Booking',
-  redirect_uri: "https://#{ENV['HTTP_HOST']}"
-)
+if Rails.env.development? || ENV['HTTP_HOST'] =~ /dev|staging/i
+  nomis = Doorkeeper::Application.find_or_create_by!(
+    name: 'NOMIS',
+    redirect_uri: "https://#{ENV['HTTP_HOST']}",
+    scopes: 'public write'
+  )
 
-mtp = Doorkeeper::Application.find_or_create_by!(
-  name: 'Money to Prisoners',
-  redirect_uri: "https://#{ENV['HTTP_HOST']}"
-)
+  pvb = Doorkeeper::Application.find_or_create_by!(
+    name: 'Prison Visits Booking',
+    redirect_uri: "https://#{ENV['HTTP_HOST']}"
+  )
 
-mps = Doorkeeper::Application.find_or_create_by!(
-  name: 'Moving People Safely',
-  redirect_uri: "https://#{ENV['HTTP_HOST']}"
-)
+  mtp = Doorkeeper::Application.find_or_create_by!(
+    name: 'Money to Prisoners',
+    redirect_uri: "https://#{ENV['HTTP_HOST']}"
+  )
+
+  mps = Doorkeeper::Application.find_or_create_by!(
+    name: 'Moving People Safely',
+    redirect_uri: "https://#{ENV['HTTP_HOST']}"
+  )
+end
 
 # Import nicknames
 file = Rails.root.join('db', 'nicknames.csv')
