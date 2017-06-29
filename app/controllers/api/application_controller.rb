d03c6b98a817538681da6a37d952b79fbde5cefb
Himal Mandalia <himalmandalia@gmail.com>
2017-06-29 17:14:18 +0100

    Set cache headers to prevent caching by browsers

--- app/controllers/api/application_controller.rb
+++ app/controllers/api/application_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class ApplicationController < ActionController::Base
+    include CacheHeaders
+
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :null_session
